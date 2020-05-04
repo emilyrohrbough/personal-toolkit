29fad2b4765d0a923099694fd4d081e77f381159
Emily Rohrbough <emily.rohrbough@cerner.com>
2020-05-03 20:15:22 -0500

    copy script

--- scripts/copy-screenshots.js
+++ scripts/copy-screenshots.js
@@ -1,12 +1,16 @@
 const fs = require('fs-extra');
 const glob = require('glob');
 
+// const referenceScreenshots = glob.sync(`${process.cwd()}/**/__snapshots__/reference/${program.theme}${program.locale}/**`, { nodir: true, ignore: '**/node_modules/**' });
 const referenceScreenshots = glob.sync(`${process.cwd()}/**/__snapshots__/reference/**`, { nodir: true, ignore: '**/node_modules/**' });
 
 const updatedScreenshots = [];
 const staleScreenshots = [];
 const diffScreenshots = [];
 
+if (!referenceScreenshots.length) {
+  console.log('no reference screenshots found.');
+}
 /**
  * Find all Reference screenshots. Check for stale screenshots & for diff screenshots.
  * Replace the Reference screenshots with the latest screenshots.
@@ -14,7 +18,7 @@ const diffScreenshots = [];
 Promise.all(referenceScreenshots.map((referenceScreenshot) => {
   const latestScreenshot = referenceScreenshot.replace('reference', 'latest');
   const diffScreenshot = referenceScreenshot.replace('reference', 'diff');
-
+  console.log(latestScreenshot)
   if (!fs.pathExistsSync(latestScreenshot)) {
     staleScreenshots.push(referenceScreenshot);
   } else if (fs.pathExistsSync(diffScreenshot)) {
